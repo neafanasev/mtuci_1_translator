02609c25502211cdb04a42757179d5ae06323778
neafanasev <neafanasev@gmail.com>
2021-11-24 15:34:38 +0300

    done 1.1

---
+++ header.py
@@ -0,0 +1,5 @@
+headers = {
+    'accept-encoding': "application/gzip",
+    'x-rapidapi-host': "google-translate1.p.rapidapi.com",
+    'x-rapidapi-key': "5f06fc98b4msh094642aebd9283bp1ae520jsn3e4556e00b29"
+}
